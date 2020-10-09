8338639b9f224f7f202165a746c9d48bfd309834
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2020-10-09 15:37:27 -0700

    adding Christians code

--- main.c
+++ main.c
@@ -1,7 +1,53 @@
+/*******************************************************************
+Users: Christian Gordon, Brandon Holder, Tahwab Noori, Tyler Cox
+Use: Create a Unix shell that runs a series of basic commands.
+Create child processes that will execute user inputed commands.
+*******************************************************************/
 #include <stdio.h>
-//make a unix shell with the boys
-int main()
+#include <unistd.h>
+
+#define MAX LINE 80 /* The maximum length command */
+
+/*******************************************************************
+Parse function
+parses the users inut
+*******************************************************************/
+void fflush(stdout)
 {
-    printf("hello");
-    return 1;
+
+}
+
+/*******************************************************************
+execvp
+parses the users inut
+*******************************************************************/
+void execvp(array args[], string args)
+{
+
+}
+
+/*******************************************************************
+Main function
+*******************************************************************/
+int main(void)
+{
+	char* args[MAX LINE / 2 + 1]; /* command line arguments */
+	int should run = 1; /* flag to determine when to exit program */
+
+	while (should run)
+	{
+		printf("osh>");
+
+		//Call parse function
+		fflush(stdout);
+
+		/**
+		 * After reading user input, the steps are:
+		 * (1) fork a child process using fork()
+		 * (2) the child process will invoke execvp()
+		 * (3) parent will invoke wait()
+		 */
+
+	}
+	return 0;
 }
