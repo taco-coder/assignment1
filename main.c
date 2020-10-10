9d14233602800a2f48fe6b2ed94c9070f41f3aa8
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2020-10-09 18:05:26 -0700

    i like pp

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(void)
 {
 	char args[MAX_LINE / 2 + 1]; /* command line arguments */
 	int should_run = 1;			  /* flag to determine when to exit program */
-	const char *token;
+	char *token;
 	int status_code = 0;
 
 	printf("osh>");
