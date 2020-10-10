cd54e851e04ffa588d88b9d84710f7e7f2fbbc96
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2020-10-09 17:00:08 -0700

    finished?

--- main.c
+++ main.c
@@ -33,12 +33,12 @@ int main(void)
 {
 	char* args[MAX LINE / 2 + 1]; /* command line arguments */
 	int should run = 1; /* flag to determine when to exit program */
+	char* token;
 
 	while (should run)
-	{
-		printf("dioahtgresip");
-		
+	{	
 		printf("osh>");
+		scanf("%s", args);
 
 		//Call parse function
 		fflush(stdout);
